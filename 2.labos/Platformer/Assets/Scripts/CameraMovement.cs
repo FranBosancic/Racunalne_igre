54674461fbdf58aa2fbdc1b6fd923b073aad3605
Fran <fran.bosancic@tvz.hr>
2025-10-25 16:36:48 +0200

    dodane platforme, kepec i movement za kepeca

---
+++ 2.labos/Platformer/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
