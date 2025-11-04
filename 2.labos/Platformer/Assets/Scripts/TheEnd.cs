d1e179061e4038530bffd493f522034076f320e3
Fran <fran.bosancic@gmail.com>
2025-11-04 10:14:42 +0100

    Added UI for reaching the end.

---
+++ 2.labos/Platformer/Assets/Scripts/TheEnd.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TheEnd : MonoBehaviour
+{
+    public GameObject winUI;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "KepecTag")
+        {
+            Time.timeScale = 0;
+            winUI.SetActive(true);
+        }
+
+    }
+}
